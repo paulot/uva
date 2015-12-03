c7284900e8ef5789f43bd8beb0bac5f3ac1eecc8
paulot <paulo.tanaka3@gmail.com>
2015-12-03 13:55:25 -0800

    Adding spoj Fishmonger

---
+++ spoj-fishmonger.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+
+int times[51][51], toll[51][51], n, t, dptime[51][1001], dpprice[51][1001];
+
+ii solve(int ccity, int ctime) {
+  if (ccity == n-1) return make_pair(0, 0);
+  if (dpprice[ccity][ctime] != -1) return make_pair(dpprice[ccity][ctime], dptime[ccity][ctime]);
+
+  int price = INT_MAX, taken = 0;;
+  for (int i = 0; i < n; i++) {
+    if (i != ccity and ctime + times[ccity][i] <= t) {
+      ii sol = solve(i, ctime + times[ccity][i]);
+      if (sol.first != INT_MAX and sol.first + toll[ccity][i] < price) {
+        price = toll[ccity][i] + sol.first, taken = times[ccity][i] + sol.second;
+      }
+    }
+  }
+  return make_pair(dpprice[ccity][ctime] = price, dptime[ccity][ctime] = taken);
+}
+
+int main() {
+  while (cin >> n >> t and n and t) {
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> times[i][j];
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) cin >> toll[i][j];
+
+    memset(dptime, -1, sizeof dptime);
+    memset(dpprice, -1, sizeof dpprice);
+
+    // Minimize price, then time
+    ii sol = solve(0, 0);
+    cout << sol.first << ' ' << sol.second << endl;
+  }
+}
